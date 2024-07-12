d4df219015d41e262931c2e8db8a1657ba48b2e1
rttitity <159294539+rttitity@users.noreply.github.com>
2024-07-12 15:49:32 +0900

    test
    test

---
+++ acmeapp/acmeapp.js
@@ -0,0 +1,19 @@
+var http = require('http');
+const port = 80;
+
+var pjson = require('./package.json');
+
+const server == http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('ACME Corporation JAM Challenge v'+pjson.version);
+  res.write('\n\n');
+  res.write('ACME App is the pinnacle of Personal Financial Management');
+  res.write('\n\n');
+  /*res.write('UUID: '+process.env.JCS);*/
+  res.end();
+});
+
+server.listen(port, () => {
+  console.log(`ACME Corporation App running on port ${port}`);
+});
